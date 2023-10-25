75a9001996d8588bb3b21d8b3102583ee23600ad
user <user@ADMIN>
2023-10-25 10:41:19 +0300

    Добавьте файлы проекта.

---
+++ Zadanie3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        if (int.TryParse(Console.ReadLine(), out int number))
+        {
+            if (number == 5 || number == 10)
+            {
+                Console.WriteLine("Число либо равно 5, либо равно 10.");
+            }
+            else
+            {
+                Console.WriteLine("Неизвестное число.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Ошибка при вводе числа.");
+        }
+    }
+}
